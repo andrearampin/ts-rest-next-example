2cd8a4fee768d210adf5c62b96d8e6ed478ebb53
Andrea Rampin <andrea.rampin@gmail.com>
2025-12-11 22:22:27 +1100

    Enhance ESLint configuration by adding Prettier plugin and enforcing formatting rules; refactor API route handler for improved readability and maintainability.

--- src/app/api/[...ts-rest]/route.ts
+++ src/app/api/[...ts-rest]/route.ts
@@ -2,21 +2,17 @@ import { apiContract } from '@/lib/api/contract';
 import { apiRouter } from '@/lib/api/router';
 import { createNextHandler } from '@ts-rest/serverless/next';
 
-const handler = createNextHandler(
-    apiContract,
-    apiRouter,
-    {
-      jsonQuery: true,
-      responseValidation: true,
-      handlerType: 'app-router',
-    },
-  );
-  
-  export {
-    handler as GET,
-    handler as POST,
-    handler as PUT,
-    handler as PATCH,
-    handler as DELETE,
-    handler as OPTIONS,
-  };
+const handler = createNextHandler(apiContract, apiRouter, {
+  jsonQuery: true,
+  responseValidation: true,
+  handlerType: 'app-router',
+});
+
+export {
+  handler as GET,
+  handler as POST,
+  handler as PUT,
+  handler as PATCH,
+  handler as DELETE,
+  handler as OPTIONS,
+};
